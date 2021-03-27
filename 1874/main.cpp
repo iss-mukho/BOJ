d55d2d021c0dab9b62b8149ad41ad0396f845fea
iss-mukho <67003627+iss-mukho@users.noreply.github.com>
2021-03-28 00:44:07 +0900

    2021-03-28

--- 1874/main.cpp
+++ 1874/main.cpp
@@ -5,22 +5,22 @@ stack<int> store;
 int input[100001] = {};
 char output[200001] = {};
 int main() {
-	int n, idx = 1, a = 1;
+	int n, comp_idx = 1, out_idx = 1;
 	cin >> n;
 	int top = 0;
 	for (int i = 1; i <= n; i++)
 		cin >> input[i];
 	for (int i = 1; i <= n; i++) {
 		store.push(i);
-		output[a++] = '+';
+		output[out_idx++] = '+';
 		top = store.top();
-		for (int x = idx; x <= i; x++) {
-			if (top == input[idx]) {
+		for (int x = comp_idx; x <= i; x++) {
+			if (top == input[comp_idx]) {
 				store.pop();
 				if(store.size())
 					top = store.top();
-				output[a++] = '-';
-				idx++;
+				output[out_idx++] = '-';
+				comp_idx++;
 			}
 		}
 	}
