c580ead261f06a1a326dc3c63b6a2753867330d1
iss-mukho <67003627+iss-mukho@users.noreply.github.com>
2021-06-11 01:12:46 +0900

    GG

--- 2461/main.cpp
+++ 2461/main.cpp
@@ -1,59 +1,44 @@
+// gg
 #include <iostream>
 #include <queue>
 #include <vector>
 #include <algorithm>
 using namespace std;
-priority_queue<int, vector<int>, greater<int>> pq[1000];
-vector<int> cmp;
-vector<int> t;
+typedef pair<int, int> ii;
+
+priority_queue<ii, vector<ii>, greater<ii>> pq;
+vector<vector<int>> cmp;
+vector<int> idx;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	int N, M, i, j, input, temp, tempMin = 1001, minIdx, maxmin = 1000, tempMaxMin, tempMinIdx, tempMin_;
-	// 입력
+	int N, M, i, j, tempMax = 0, maxmin = 1000000001, minIdx;
+	
 	cin >> N >> M;
+	cmp = vector<vector<int>>(N, vector<int>(M));
+	idx = vector<int>(N, 0);
 	for (i = 0; i < N; ++i) {
-		for (j = 0; j < M; ++j) {
-			cin >> input;
-			pq[i].push(input);
-		}
+		for (j = 0; j < M; ++j)
+			cin >> cmp[i][j];
 	}
-	// 초기값
 	for (i = 0; i < N; ++i) {
-		temp = pq[i].top();
-		if (temp < tempMin) {
-			tempMin = temp;
-			minIdx = i;
-		}
-		pq[i].pop();
-		cmp.push_back(temp);
+		sort(cmp[i].begin(), cmp[i].end());
+		tempMax = max(tempMax, cmp[i][0]);
+		pq.push({ cmp[i][0], i });
 	}
-	t.resize((int)(cmp.size()));
-	copy(cmp.begin(), cmp.end(), t.begin());
-	while (!t.empty()) {
-		tempMin_ = *min_element(t.begin(), t.end());
-		if (tempMin_ == 1001)
+
+	for (i = 0; i < N * M - N; ++i) {
+		ii cur = pq.top();
+		minIdx = cur.second;
+		maxmin = min(maxmin, tempMax - cur.first);
+
+		if (idx[minIdx] == N - 1)
 			break;
-
-		tempMaxMin = *max_element(cmp.begin(), cmp.end()) - *min_element(cmp.begin(), cmp.end());
-		if (tempMaxMin < maxmin)
-			maxmin = tempMaxMin;
-
-		if (!pq[minIdx].empty()) {
-			cmp[minIdx] = pq[minIdx].top();
-			pq[minIdx].pop();
-			if (pq[minIdx].empty()) {
-				t[minIdx] = 1001;
-			}
-			minIdx = min_element(cmp.begin(), cmp.end()) - cmp.begin();
-		}
 		else {
-			tempMinIdx = min_element(t.begin(), t.end()) - t.begin();
-			cmp[tempMinIdx] = *min_element(t.begin(), t.end());
-			if (pq[tempMinIdx].empty())
-				t[tempMinIdx] = 1001;
-			else
-				pq[tempMinIdx].pop();
+			pq.pop();
+			++idx[minIdx];
+			tempMax = max(tempMax, cmp[minIdx][idx[minIdx]]);
+			pq.push({ cmp[minIdx][idx[minIdx]], minIdx });
 		}
 	}
 	cout << maxmin << '\n';
