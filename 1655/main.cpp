f68a6e30f7aa246c71d28b783e484883fe52e223
iss-mukho <67003627+iss-mukho@users.noreply.github.com>
2021-06-11 05:15:46 +0900

    2021-06-11

--- 1655/main.cpp
+++ 1655/main.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
-vector<int> num;
+priority_queue<int> maxpq;
+priority_queue<int, vector<int>, greater<int>> minpq;
 int main() {
+	// max(-3, -2, -1, 0) min(1, 2, 3)
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int N, i, mid, n;
+	int N, i, mid, input, temp1, temp2;
 	cin >> N;
 	for (i = 0; i < N; ++i) {
-		cin >> n;
-		num.push_back(n);
-		sort(num.begin(), num.end());
-		mid = num.size() % 2 == 1 ? num.size() / 2 : num.size() / 2 - 1;
-		cout << num[mid] << '\n';
+		cin >> input;
+		if (i%2 == 0)
+			maxpq.push(input);
+		else
+			minpq.push(input);
+		if (i > 0) {
+			if (minpq.top() < maxpq.top()) {
+				temp1 = maxpq.top();
+				temp2 = minpq.top();
+				maxpq.pop(); minpq.pop();
+				maxpq.push(temp2); minpq.push(temp1);
+			}
+		}
+		cout << maxpq.top() << '\n';
 	}
 	return 0;
 }
