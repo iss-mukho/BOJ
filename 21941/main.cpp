40890b7c358cb4d3f9073601f364d46ab735459c
iss-mukho <67003627+iss-mukho@users.noreply.github.com>
2021-06-19 21:19:26 +0900

    2021-06-19
    why

--- 21941/main.cpp
+++ 21941/main.cpp
@@ -1,59 +1,63 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
 
-int dp[101];
-map<string, int> input;
-map<string, int> input_len;
-int dfs(int idx, string in);
+struct mukho {
+	int score;
+	int len;
+};
+
+string S; int N;
+int dp[1001];
+unordered_map<string, mukho> input; string in[100];
+int dfs(int idx);
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	fill_n(dp, 101, -1);
-	string S, ts;
-	int M, i, score = 0, ti;
+	int M, i, X, temp;
+	string A;
 	
 	// 입력
 	cin >> S;
 	cin >> M;
+	N = S.length();
+	fill_n(dp, N+1, -1);
 	for (i = 0; i < M; ++i) {
-		cin >> ts >> ti;
-		input.insert(make_pair(ts, ti));
-		input_len.insert(make_pair(ts, ts.length()));
-	}	
+		cin >> A >> X;
+		temp = A.length(); mukho t = { X, temp };
+		input[A] = t; in[i] = A;
+	}		
 
 	// 출력
-	cout << dfs(0, S) << '\n';
+	cout << dfs(0) << '\n';
 	return 0;
 }
 
-int dfs(int idx, string in) {
-	if (idx >= in.length())
+int dfs(int idx) {
+	if (idx >= N)
 		return 0;
 	if (dp[idx] != -1)
 		return dp[idx];
 
-	int max_value = 0, i, j, ti, tsl;
-	map<string, int>::iterator iter1 = input.begin();
-	map<string, int>::iterator iter2 = input_len.begin();
-	for (i = 0; i < input.size(); ++i) {
-		ti = (*iter1).second; tsl = (*iter2).second;
+	string str;
+	int max_value = 0, i, score, len;
 
-		if (idx + tsl - 1 < in.length()) {
-			for (j = 0; j < tsl; ++j) {
-				if (in[j + idx] != (*iter1).first[j])
+	for (string str: in) {
+		score = input[str].score; len = input[str].len;
+
+		if (idx + len - 1 < N) {
+			for (i = 0; i < len; ++i) {
+				if (S[i + idx] != str[i])
 					break;
 				else
-					max_value = max(max_value, ti + dfs(idx + tsl, in));
+					max_value = max(max_value, score + dfs(idx + len));
 			}
 		}
-
-		++iter1; ++iter2;
 	}
 
-	max_value = max(max_value, 1 + dfs(idx + 1, in));
+	max_value = max(max_value, 1 + dfs(idx + 1));
 	dp[idx] = max_value;
 	return dp[idx];
 }
