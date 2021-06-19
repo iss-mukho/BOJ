1897a9f351e011a5cc2a0a760b0b5af6686079b5
iss-mukho <67003627+iss-mukho@users.noreply.github.com>
2021-06-19 19:52:43 +0900

    2021-06-19
    fail

--- 21941/main.cpp
+++ 21941/main.cpp
@@ -1,55 +1,59 @@
 #include <iostream>
 #include <string>
-#include <utility>
+#include <map>
 #include <algorithm>
 using namespace std;
-// 정렬함수 선언
-bool cmp(pair<string, int> a, pair<string, int> b);
+
+int dp[101];
+map<string, int> input;
+map<string, int> input_len;
+int dfs(int idx, string in);
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	pair<string, int> input[100];
-	string S;
-	int M, i, score = 0;
+	fill_n(dp, 101, -1);
+	string S, ts;
+	int M, i, score = 0, ti;
 	
 	// 입력
 	cin >> S;
 	cin >> M;
-	for (i = 0; i < M; ++i)
-		cin >> input[i].first >> input[i].second;
-	sort(input, input + M, cmp);
-	
-	// 알고리즘
 	for (i = 0; i < M; ++i) {
-		if (input[i].second <= input[i].first.length())
-			continue;
-		while (true) {
-			if (S.find(input[i].first) != -1) {
-				S.erase(S.find(input[i].first), input[i].first.length());
-				score += input[i].second;
-			}
-			else
-				break;
-		}
-	}
-	score += S.length();
+		cin >> ts >> ti;
+		input.insert(make_pair(ts, ti));
+		input_len.insert(make_pair(ts, ts.length()));
+	}	
 
 	// 출력
-	cout << score << '\n';
+	cout << dfs(0, S) << '\n';
 	return 0;
 }
-// 정렬함수 정의
-bool cmp(pair<string, int> a, pair<string, int> b) {
-	double as = a.second / a.first.length();
-	double bs = b.second / b.first.length();
-	if (as > bs)
-		return true;
-	else if (as == bs) {
-		if (a.second >= b.second)
-			return true;
-		else
-			return false;
+
+int dfs(int idx, string in) {
+	if (idx >= in.length())
+		return 0;
+	if (dp[idx] != -1)
+		return dp[idx];
+
+	int max_value = 0, i, j, ti, tsl;
+	map<string, int>::iterator iter1 = input.begin();
+	map<string, int>::iterator iter2 = input_len.begin();
+	for (i = 0; i < input.size(); ++i) {
+		ti = (*iter1).second; tsl = (*iter2).second;
+
+		if (idx + tsl - 1 < in.length()) {
+			for (j = 0; j < tsl; ++j) {
+				if (in[j + idx] != (*iter1).first[j])
+					break;
+				else
+					max_value = max(max_value, ti + dfs(idx + tsl, in));
+			}
+		}
+
+		++iter1; ++iter2;
 	}
-	else
-		return false;
+
+	max_value = max(max_value, 1 + dfs(idx + 1, in));
+	dp[idx] = max_value;
+	return dp[idx];
 }
