48df89362166d0cdf65ebc336828669e4a6b48a8
고승완 <komogoon@gmail.com>
2023-03-15 10:10:08 +0900

    Update 2961

--- BOJ_2961/src/Main.java
+++ BOJ_2961/src/Main.java
@@ -8,12 +8,12 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N;
-    static long[] so; // 신맛
-    static long[] ni; // 쓴맛
+    static int[] so; // 신맛
+    static int[] ni; // 쓴맛
     static boolean[] isUsed;
-    static ArrayList<Long> savedSo = new ArrayList<>();
-    static ArrayList<Long> savedNi = new ArrayList<>();
-    static long answer = Long.MAX_VALUE;
+    static ArrayList<Integer> savedSo = new ArrayList<>();
+    static ArrayList<Integer> savedNi = new ArrayList<>();
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,8 +29,8 @@ public class Main {
         for(int i=0; i<N; ++i){
             st = new StringTokenizer(br.readLine());
 
-            so[i] = Long.parseLong(st.nextToken());
-            ni[i] = Long.parseLong(st.nextToken());
+            so[i] = Integer.parseInt(st.nextToken());
+            ni[i] = Integer.parseInt(st.nextToken());
         }
 
         // 처리
@@ -45,20 +45,20 @@ public class Main {
     }
 
     public static void initArrays(){
-        so = new long[N];
-        ni = new long[N];
+        so = new int[N];
+        ni = new int[N];
         isUsed = new boolean[N];
     }
 
-    public static long getSo(){
-        long result = 1;
+    public static int getSo(){
+        int result = 1;
         for(var num: savedSo)
             result *= num;
         return result;
     }
 
-    public static long getNi(){
-        long result = 0;
+    public static int getNi(){
+        int result = 0;
         for(var num: savedNi)
             result += num;
         return result;
@@ -66,8 +66,8 @@ public class Main {
 
     public static void getAnswer(int index, int count, int maxCount){
         if(count == maxCount){
-            long tempAnswer = Math.abs(getSo() - getNi());
-            answer = Long.min(answer, tempAnswer);
+            int tempAnswer = Math.abs(getSo() - getNi());
+            answer = Integer.min(answer, tempAnswer);
             return;
         }
 
