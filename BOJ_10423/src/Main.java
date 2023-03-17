9cbca010ccad433b2e3a03aa9a84d731f4a96f0a
고승완 <komogoon@gmail.com>
2023-03-17 16:07:39 +0900

    10423

--- BOJ_10423/src/Main.java
+++ BOJ_10423/src/Main.java
@@ -1,36 +1,47 @@
-// 백준 10423: 전기가 부족해(골드 II), https://www.acmicpc.net/problem/10423
+/**
+ * 백준 10423: 전기가 부족해(골드 II), https://www.acmicpc.net/problem/10423
+ * 맞왜틀? -> (풀이1) 다익스트라를 발전소 기준으로 시작..? 왜요?
+ *       -> (풀이2): https://steady-coding.tistory.com/123
+ *              - 발전소의 부모 노드를 0으로 두고, 크루스칼 알고리즘을 돌리면서 모든 도시의 루트 노드가 0이면 종료
+ *              - 발전소의 부모 노드를 0으로 두게 되면, cycle이 없는 MST를 만드는 크루스칼 알고리즘의 특성에 따라 발전소끼리 연결되지 않음
+ * 그래서 대체 반례가 뭘까 -> 진짜 모름
+ */
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N, M, K, u, v, w;
+    static int N, M, K, u, v, w, answer = 0;
+    static ArrayList<EDGE> edges = new ArrayList<>();
     static int[] parent;
     static boolean[] isPower;
-    static ArrayList<EDGE> edges = new ArrayList<>();
-    static int answer = 0, count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        // 입력
+        // 입력 1
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
+        // 초기화
         parent = new int[N+1];
         isPower = new boolean[N+1];
         for(int i=1; i<=N; ++i) parent[i] = i;
 
+        // 입력 2
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<K; ++i) isPower[Integer.parseInt(st.nextToken())] = true;
-        count = N - K;
+        for(int i=0; i<K; ++i){
+            int p = Integer.parseInt(st.nextToken());
+            isPower[p] = true;
+            parent[p] = 0; // add
+        }
 
+        // 입력 3
         for(int i=0; i<M; ++i){
             st = new StringTokenizer(br.readLine());
             u = Integer.parseInt(st.nextToken());
@@ -53,9 +64,7 @@ public class Main {
     }
 
     public static class EDGE implements Comparable<EDGE>{
-        int s;
-        int e;
-        int w;
+        int s; int e; int w;
 
         public EDGE(int s, int e, int w) {
             this.s = s;
@@ -75,40 +84,23 @@ public class Main {
     }
 
     public static void unionParent(int a, int b) {
-        a = findParent(a);
-        b = findParent(b);
-
+        a = findParent(a); b = findParent(b);
         if (a < b) parent[b] = a;
         else parent[a] = b;
     }
 
-    public static void swap(int a, int b){
-        int temp = a;
-        a = b;
-        b = temp;
+    public static boolean areAllPowered(){
+        for(int i=1; i<=N; ++i)
+            if(!isPower[i]) return false;
+        return true;
     }
 
     public static void kruskal(){
         for(var edge: edges){
+            if(areAllPowered()) break;
+
             u = edge.s; v = edge.e; w = edge.w;
-            if(u > v) swap(u, v); // 구현 상의 편의를 위해 (u < v)
-
-            if(isPower[u] && isPower[v]) continue; // 연결 안해도 됨
             if(findParent(u) == findParent(v)) continue; // this is cycle
-
-            if(isPower[u] || isPower[v]){
-                // 기존에 연결되어 있는 도시 중 false인 경우를 갱신시키기 위해 true 연결
-                int p = u;
-                while(true){
-                    if(parent[p] == p) break;
-
-                    isPower[parent[p]] = true;
-                    p = parent[p];
-                }
-
-                isPower[u] = true;
-                isPower[v] = true;
-            }
 
             unionParent(u, v);
             answer += w;
