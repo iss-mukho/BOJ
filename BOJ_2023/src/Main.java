e3de8bb8fbe6309d10704424fd62180fbb1c1acf
고승완 <komogoon@gmail.com>
2023-03-20 15:48:02 +0900

    2023 주석 추가

--- BOJ_2023/src/Main.java
+++ BOJ_2023/src/Main.java
@@ -1,12 +1,10 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
 // 백준 2023: 신기한 소수(골드 V),https://www.acmicpc.net/problem/2023
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
 
-    static int N, MIN, MAX;
+    static int N;
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
@@ -22,6 +20,7 @@ public class Main {
     public static boolean isPrimeNumber(int number){
         if(number == 1) return false;
 
+        // MAX 10000, 에라토스테네스의 체
         for(int i=2; i<Math.sqrt(number); ++i)
             if(number % i == 0)
                 return false;
@@ -36,10 +35,10 @@ public class Main {
 
         for(int i=1; i<10; i+=2){
             if(i == 5) continue;
+
             int temp = number * 10 + i;
             if(!isPrimeNumber(temp)) continue;
             checkPrimeNumber(temp, count+1);
         }
-
     }
 }
