769d93c93adb21d1f763f072fd81020ccd6e7cb6
고승완 <komogoon@gmail.com>
2023-03-21 19:19:58 +0900

    1991

--- Python/1629.py
+++ Python/1629.py
@@ -1,6 +1,14 @@
 # 백준 1629: 곱셈(실버 I), https://www.acmicpc.net/problem/1629
 A, B, C = map(int, input().split())
 
-if A % 2 == 0:
-    print(A % C)
-else: print(A*A % C)
+def check(a, b):
+    if b == 1:
+        return a % C
+    elif b % 2 == 0:
+        x = check(a, b // 2)
+        return (x*x) % C
+    else:
+        x = check(a, (b-1) // 2)
+        return (x*x) * a % C
+
+print(check(A, B))
