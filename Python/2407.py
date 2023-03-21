7849a16a80582c76d6b726f35a1f92394dd97668
고승완 <komogoon@gmail.com>
2023-03-21 18:36:38 +0900

    2407

---
+++ Python/2407.py
@@ -0,0 +1,11 @@
+# 백준 2407: 조합(실버 III), https://www.acmicpc.net/problem/2407
+N, M = map(int, input().split())
+
+a = 1
+b = 1
+for i in range(N, N-M, -1):
+    a *= i
+for i in range(M, 0, -1):
+    b *= i
+
+print(int(a/b))
