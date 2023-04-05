1eac63a05c10d97a8c3bbe9c101fb820140c158a
Seungwan <mukho@kakao.com>
2023-04-06 06:17:33 +0900

    8545

---
+++ 1_cpp/8545/main.cpp
@@ -0,0 +1,14 @@
+﻿// 백준 8545: Zadanie próbne(브론즈 V), https://www.acmicpc.net/problem/8545
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	reverse(str.begin(), str.end());
+	cout << str << '\n';
+
+	return 0;
+}
