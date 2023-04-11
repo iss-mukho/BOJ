48e735ff3c11fe4a1aceeeddd4ae492f20c94644
고승완 <komogoon@gmail.com>
2023-04-11 17:54:02 +0900

    Update 11048

--- 2_python/11048.py
+++ 2_python/11048.py
@@ -2,26 +2,13 @@
 import sys
 input = sys.stdin.readline
 
-dx = [-1, +0, -1]
-dy = [+0, -1, -1]
-def inRange(r, c):
-    return 0 <= r < N and 0 <= c < M
-
 N, M = map(int, input().split())
-maze = []
+maze = [[0] * (M+1)]
 for _ in range(N):
-    maze.append(list(map(int, input().split())))
+    maze.append([0] + list(map(int, input().split())))
 
-dp = [[0 for _ in range(M)] for _ in range(N)]
-dp[0][0] = maze[0][0]
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        maze[i][j] += max(maze[i-1][j], maze[i-1][j-1], maze[i][j-1])
 
-for i in range(N):
-    for j in range(M):
-        for k in range(3):
-            bi = i + dx[k]
-            bj = j + dy[k]
-
-            if(not inRange(bi, bj)): continue
-            dp[i][j] = max(dp[i][j], dp[bi][bj] + maze[i][j])
-
-print(dp[N-1][M-1])
+print(maze[N][M])
