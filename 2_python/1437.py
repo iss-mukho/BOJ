06ac1c612a2f72388e8f5a555d5d5130fcb3bf9e
고승완 <komogoon@gmail.com>
2023-04-13 22:27:29 +0900

    Add another rule in 1437

--- 2_python/1437.py
+++ 2_python/1437.py
@@ -1,6 +1,6 @@
 # 백준 1437: 수 분해(골드 IV), https://www.acmicpc.net/problem/1437
 MOD = 10007
-dp = [0] * (1000001)
+dp = [0] * 1000001
 dp[0] = 0
 dp[1] = 1
 dp[2] = 2
@@ -8,7 +8,11 @@ dp[3] = 3
 
 N = int(input())
 for i in range(4, N+1):
+    # 자체 풀이
     num = pow(3, (i-4)//3) * ((i-1) % 3 + 1)
     dp[i] = (dp[i-1] + num) % MOD
+
+    # 다른 규칙
+    dp[i] = (2 * dp[i-2]) % MOD if i%3 else (3 * dp[i-3]) % MOD
 
 print(dp[N])
