0c80d2dc0a182ec8663661e44734ae9c31120ed4
Seungwan <mukho@kakao.com>
2023-04-14 00:54:25 +0900

    24266

---
+++ 1_cpp/24266/main.cpp
@@ -0,0 +1,11 @@
+// 백준 24266: 알고리즘 수업 - 알고리즘의 수행 시간 5(브론즈 III), https://www.acmicpc.net/problem/24266
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long n;
+	cin >> n;
+	cout << n * n * n << '\n' << 3 << '\n';
+
+	return 0;
+}
