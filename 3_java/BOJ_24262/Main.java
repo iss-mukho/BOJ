da2b98ca12379ea84764508c500d8e22d24a267e
Seungwan <mukho@kakao.com>
2023-04-14 00:40:35 +0900

    24262

---
+++ 3_java/BOJ_24262/Main.java
@@ -0,0 +1,10 @@
+// 백준 24262: 알고리즘 수업 - 알고리즘의 수행 시간1(브론즈 V), https://www.acmicpc.net/problem/24262
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("1\n0\n");
+    }
+}
