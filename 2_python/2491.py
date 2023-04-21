27024d6bdc8128a0a5378148c4391d0d5a2139ba
고승완 <komogoon@gmail.com>
2023-04-21 20:36:25 +0900

    2491

---
+++ 2_python/2491.py
@@ -0,0 +1,15 @@
+# 백준 2491: 수열(실버 IV), https://www.acmicpc.net/problem/2491
+import sys
+input = sys.stdin.readline
+
+answer = 0
+N = int(input())
+number = list(map(int, input().split()))
+
+u = d = 1
+for i in range(1, N):
+    u = u+1 if number[i-1] <= number[i] else 1
+    d = d+1 if number[i-1] >= number[i] else 1
+    answer = max(answer, u, d)
+
+print(answer)
