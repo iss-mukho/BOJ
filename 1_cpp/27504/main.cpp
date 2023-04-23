19960e90926c847effb05fd98d41618fc3e17d7c
Mukho <mukho@kakao.com>
2023-04-24 08:44:24 +0900

    AC 27504

--- 1_cpp/27504/main.cpp
+++ 1_cpp/27504/main.cpp
@@ -1,39 +1,31 @@
-// ¹éÁØ 27504: »ó´ëÀ½°¨ÀÇ ³ë·¡Ã£±â(ÇÃ·¡Æ¼³Ñ V), https://www.acmicpc.net/problem/27504
+// ë°±ì¤€ 27504: ìƒëŒ€ìŒê°ì˜ ë…¸ë˜ì°¾ê¸°(í”Œë˜í‹°ë„˜ V), https://www.acmicpc.net/problem/27504
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 #include <vector>
-#include <algorithm>
 using namespace std;
 const int MAX = 1000000;
 
-int N, K, n, L;
-vector<int> kText;
+int N, K, m, L;
+vector<int> melody;
 vector<vector<int>> texts;
 vector<int> pattern;
 int failTable[MAX];
 vector<int> answer;
 
-bool KMP(vector<int> text, vector<int> &pattern) {
+bool KMP(vector<int> &text, vector<int> &pattern) {
 	int j = 0;
-	for (int i = 1; i < L; ++i) {
+	for (int i = 1; i < (signed)pattern.size(); ++i) {
 		while (j > 0 && pattern[i] != pattern[j])
 			j = failTable[j - 1];
 		if (pattern[i] == pattern[j])
 			failTable[i] = ++j;
 	}
 
-	int base = text[0];
 	j = 0;
 	for (int i = 0; i < (signed)text.size(); ++i) {
-		int conv = text[i] - base + 1;
-
-		while (j > 0 && conv != pattern[j]) {
+		while (j > 0 && text[i] != pattern[j])
 			j = failTable[j - 1];
-			base = text[i + j] - pattern[j] + 1; // base °ª ÀçÁ¶Á¤
-			conv = text[i] - base + 1; // »ó´ëÀûÀÎ Å©±â ´Ù½Ã °è»ê
-		}
-			
-		if (conv == pattern[j]) {
+		if (text[i] == pattern[j]) {
 			++j;
 			if (j == (signed)pattern.size())
 				return true;
@@ -41,24 +33,32 @@ bool KMP(vector<int> text, vector<int> &pattern) {
 	}
 
 	return false;
- }
+}
+
+vector<int> getPreprocessMelody(vector<int> &v){
+    vector<int> pre;
+    for(int i = 0; i < (signed)v.size() - 1; ++i)
+        pre.push_back(v[i] - v[i + 1]);
+    return pre;
+}
 
 void input() {
 	int _ = scanf("%d", &N);
 	for (int i = 0; i < N; ++i) {
 		_ = scanf("%d", &K);
 		for (int j = 0; j < K; ++j) {
-			_ = scanf("%d", &n);
-			kText.push_back(n);
+			_ = scanf("%d", &m);
+			melody.push_back(m);
 		}
-		texts.push_back(kText);
-		kText.clear();
+		texts.push_back(getPreprocessMelody(melody));
+		melody.clear();
 	}
 	
 	_ = scanf("%d", &L);
-	for (int i = 0; i < L; ++i) { 
-		_ = scanf("%d", &n);
-		pattern.push_back(n);
+	for (int i = 0; i < L; ++i) {
+		_ = scanf("%d", &m);
+		melody.push_back(m);
+		pattern = getPreprocessMelody(melody);
 	}
 }
 
@@ -76,8 +76,8 @@ void output() {
 		return;
 	}
 		
-	for (auto& n : answer)
-		printf("%d ", n);
+	for (auto& a : answer)
+		printf("%d ", a);
 	printf("\n");
 }
 
