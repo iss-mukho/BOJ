4e16b657dc6bd7f1ba5fded2255587d218727a21
Seungwan <mukho@kakao.com>
2023-05-06 12:13:44 +0900

    16394

---
+++ 1_cpp/16394/main.cpp
@@ -0,0 +1,14 @@
+// 백준 16394: 홍익대학교(브론즈 V), https://www.acmicpc.net/problem/16394
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	cout << N - 1945 - 1 << '\n';
+
+	return 0;
+}
