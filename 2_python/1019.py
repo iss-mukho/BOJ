0958a6cb21fb8b212af04912a4ed10638c0a2d1a
Mukho <mukho@kakao.com>
2023-05-23 21:16:13 +0900

    Update 1019

--- 2_python/1019.py
+++ 2_python/1019.py
@@ -1,4 +1,4 @@
-# 백준: 책 페이지(골드 I), https://www.acmicpc.net/problem/1019
+# 백준 1019: 책 페이지(골드 I), https://www.acmicpc.net/problem/1019
 def calc(page, number, index):
     while page > 0:
         number[page % 10] += index
@@ -6,17 +6,18 @@ def calc(page, number, index):
 
 N = int(input())
 number = [0] * 10
-
 start = 1
 index = 1
 
 while start <= N:
+    # 페이지 번호 일의 자리를 9로
     while N % 10 != 9 and start <= N:
         calc(N, number, index)
         N -= 1
     
     if N < start: break
 
+    # 시작 번호의 일의 자리를 0으로
     while start % 10 != 0 and start <= N:
         calc(start, number, index)
         start += 1
@@ -24,6 +25,7 @@ while start <= N:
     start //= 10
     N //= 10
 
+    # 규칙성
     for i in range(10):
         number[i] += (N - start + 1) * index
     
