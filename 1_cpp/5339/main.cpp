3a0cb30795c28b0c44f7ecd73cbe9a052f897f96
Mukho <mukho@kakao.com>
2023-06-09 12:39:55 +0900

    5339

---
+++ 1_cpp/5339/main.cpp
@@ -0,0 +1,18 @@
+// 백준 5339: 콜센터(브론즈 V), https://www.acmicpc.net/problem/5339
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "     /~\\" << '\n';
+	cout << "    ( oo|" << '\n';
+	cout << "    _\\=/_" << '\n';
+	cout << "   /  _  \\" << '\n';
+	cout << "  //|/.\\|\\\\" << '\n';
+	cout << " ||  \\ /  ||" << '\n';
+	cout << "============" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+
+	return 0;
+}
