97f9ef30f5fce267b27d05469f635b6b527cff6b
Mukho <mukho@kakao.com>
2023-06-17 08:39:38 +0900

    1000

--- 5_kotlin/15740.kt
+++ 5_kotlin/15740.kt
@@ -1,5 +1,5 @@
 import java.util.*
 
-fun main(args: Array<String>) = with Scanner(System.`in`) {
+fun main(args: Array<String>) = with(Scanner(System.`in`)) {
     println(nextBigInteger() + nextBigInteger())
 }
