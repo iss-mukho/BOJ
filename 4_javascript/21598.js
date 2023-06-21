fb68e4099a86a28950bd07349c4ef7bc5cad5102
Mukho <mukho@kakao.com>
2023-06-21 10:22:41 +0900

    21598

---
+++ 4_javascript/21598.js
@@ -0,0 +1,7 @@
+// 백준 21598: SciComLove(브론즈 V), https://www.acmicpc.net/problem/21598
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+for(let i = 0; i < input; ++i) {
+    console.log('SciComLove');
+}
