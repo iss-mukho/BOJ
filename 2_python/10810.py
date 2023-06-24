3f5d877094130943a6e3a3ac04e777308f9942a2
Mukho <mukho@kakao.com>
2023-06-24 13:30:51 +0900

    10810

---
+++ 2_python/10810.py
@@ -0,0 +1,10 @@
+# 백준 10810: 공 넣기(브론즈 III)
+N, M = map(int, input().split())
+
+basket = [0] * (N)
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    for idx in range(i, j + 1):
+        basket[idx - 1] = k
+
+print(*basket)
