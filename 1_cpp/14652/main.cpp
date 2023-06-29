a23e700a5d9da84dd69c42b4bb88993bb424c764
Mukho <mukho@kakao.com>
2023-06-29 11:46:40 +0900

    14652

---
+++ 1_cpp/14652/main.cpp
@@ -0,0 +1,11 @@
+// 백준 14652: 나는 행복합니다~(브론즈 V), https ://www.acmicpc.net/problem/14652
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, M, K;
+	cin >> N >> M >> K;
+	cout << K / M << ' ' << K % M << '\n';
+
+	return 0;
+}
