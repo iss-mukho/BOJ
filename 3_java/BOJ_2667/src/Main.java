5da2e1cdd31e25944ad5a5e2b1ba33e04a6e7741
Mukho <mukho@kakao.com>
2023-07-21 06:06:20 +0900

    Fix 2667

--- 3_java/BOJ_2667/src/Main.java
+++ 3_java/BOJ_2667/src/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -75,6 +76,7 @@ public class Main {
 			}
 		}
 
+		Collections.sort(answer);
 		bw.write(answer.size() + "\n");
 		for (int ans : answer)
 			bw.write(ans + "\n");
