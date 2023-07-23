7e0967b3a180fe14b8a65521322d53f248b6bafe
Mukho <mukho@kakao.com>
2023-07-23 14:45:15 +0900

    1927

---
+++ 3_java/BOJ_1927/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int N = Integer.parseInt(br.readLine());
+
+		while (N-- > 0) {
+			int x = Integer.parseInt(br.readLine());
+
+			if (x == 0) {
+				if (pq.isEmpty())
+					bw.write(0 + "\n");
+				else
+					bw.write(pq.poll() + "\n");
+				continue;
+			}
+			pq.add(x);
+		}
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
