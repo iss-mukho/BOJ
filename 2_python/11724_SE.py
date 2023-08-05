34bfc723bf521a7989e91b9dd021cea163d4e4ec
Mukho <mukho@kakao.com>
2023-08-05 11:51:51 +0900

    11724 SE

---
+++ 2_python/11724_SE.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+
+def dfs(v):
+    visited[v] = True
+    for w in graph[v]:
+        if not visited[w]:
+            dfs(w)
+
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+answer = 0
+for i in range(1, N + 1):
+    if not visited[i]:
+        dfs(i)
+        answer += 1
+
+print(answer)
